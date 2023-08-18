885490b30a853d07558cf1e0cc8d66fff2c981b7
Allison Sobol <allisonsobol13@gmail.com>
2023-08-18 15:14:48 -0700

    Set up/base for the project

---
+++ main/main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, '../browser/index.html'));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+  });
+
+  app.setAppUserModelId("Danco Scheduler ;)");
---
+++ renderer/renderer.js
@@ -0,0 +1,11 @@
+import { BrowserWindow } from 'electron';
+
+// Create a new window.
+const mainWindow = new BrowserWindow({
+  width: 800,
+  height: 600,
+  title: 'Danco Scheduler',
+});
+
+// Load the index.html file into the window.
+mainWindow.loadFile('index.html');
