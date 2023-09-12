f454004f3f619a52d8c3a8e270d92d92f6949524
Allison Sobol <allisonsobol13@gmail.com>
2023-09-12 14:55:12 -0700

    Calendar structure

--- components/Calendar.js
+++ components/Calendar.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Calendar = () => {
     return(
-        <div>
-            <h5>CALENDAR</h5>
+        <div className="wrapper">
+            <header>
+                <p className="currentMonth">September 2023</p>
+                <div className="icons">
+                    <p>Add icons here</p>
+                </div>
+            </header>
+            <div className="calendar">
+                <ul class="weeks">
+                    <li>Sun</li>
+                    <li>Mon</li>
+                    <li>Tue</li>
+                    <li>Wed</li>
+                    <li>Thu</li>
+                    <li>Fri</li>
+                    <li>Sat</li>
+                </ul>
+                <ul className="days">
+                    <li>#</li>
+                </ul>
+            </div>
         </div>
     );
 };
