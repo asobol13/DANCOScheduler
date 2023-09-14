09e21b128436018e441f86aa5a725dd57d0f2129
Allison Sobol <allisonsobol13@gmail.com>
2023-09-14 15:58:09 -0700

    Renamed to match casing

--- app/requests.js
+++ app/requests.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Request from '../components/requestsPage.js';
+import Request from '../components/RequestsPage.js';
 
 // Use createRoot instead of ReactDOM.render
 const rootElement3 = document.getElementById('root3');
