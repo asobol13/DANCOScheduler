277f94bba9659b13a8a20cfde83c84af4dbec27c
Allison Sobol <allisonsobol13@gmail.com>
2023-09-20 14:38:14 -0700

    navbar divs

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,12 +5,20 @@ const Navbar = () => {
         <div className="sideNav" style={{ height: '100%', 
         width: '25%', position: 'fixed', zIndex: '1', top: '0',
         left: '0', backgroundColor: '#232323', transition: '.5 ease',
-        overflowX: 'hidden', paddingTop: '20px', display: 'flex', 
-        flexDirection: 'column', alignItems: 'center'}}>
-            <a href="#section">Home</a>
-            <a href="#section">Requests</a>
-            <a href="#section">Maintenance</a>
-            <a href="#section">Logout</a>
+        overflowX: 'hidden', paddingTop: '20px', paddingBottom: '20px', 
+        display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <div className="sectionOne" style={{ padding: '20px', color: 'white'}}>
+                <a href="#section">Home</a>
+            </div>
+            <div className="sectionTwo" style={{ padding: '20px', color: 'white'}}>
+                <a href="#section">Requests</a>
+            </div>
+            <div className="sectionThree" style={{ padding: '20px', color: 'white'}}>
+                <a href="#section">Maintenance</a>
+            </div>
+            <div className="sectionFour" style={{ padding: '20px', color: 'white'}}>
+                <a href="#section">Logout</a>
+            </div>
         </div>
     );
 };
